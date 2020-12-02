c0f1800a9b6f3c137f2b957144b78b776c2a275e
Erin Sparling <erin@everyplace.net>
2020-12-01 23:50:00 -0500

    proper use of replaceAll

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = function(){
 
-		document.body.innerHTML = document.body.innerHTML.replaceall('Yale', 'Yale 🏫');
-		document.body.innerHTML = document.body.innerHTML.replaceall('YALE', 'YALE 🏫');
-		document.body.innerHTML = document.body.innerHTML.replaceall('yale', 'yale 🏫');
+		document.body.innerHTML = document.body.innerHTML.replaceAll('Yale', 'Yale 🏫');
+		document.body.innerHTML = document.body.innerHTML.replaceAll('YALE', 'YALE 🏫');
+		document.body.innerHTML = document.body.innerHTML.replaceAll('yale', 'yale 🏫');
 
 		document.querySelector("body").insertAdjacentHTML("beforeend",`<h1 class="testing">😔 testing!!!</h1>`)
 	
